e5b81744c8c89d9f3c138d28178125bc3e35fa6f
Benjamin <benjamin.poell@outlook.com>
2023-05-13 18:17:37 +0200

    Upgraded to flutter 3.10 and fixed problems

--- test/boardview_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:boardview/boardview.dart';
-
-void main() {
-  test('adds one to input values', () {
-    final boardview = BoardView();
-  });
-}
